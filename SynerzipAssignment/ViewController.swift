dd3e3eb7b55183eb019f79b34148634b0c047ca1
Gaurav Thorat <grthorat@gmail.com>
2020-08-29 15:54:34 +0530

    Initial Commit

---
+++ SynerzipAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SynerzipAssignment
+//
+//  Created by gaurav thorat on 29/08/20.
+//  Copyright © 2020 gaurav thorat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
